5d9710b58084fecbdfdb77cbf6f1c67a29d53833
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-04-24 14:23:57 +0900

    feat とりあえずMVC

---
+++ backend/resources/views/components/newvisitor.blade.php
@@ -0,0 +1,10 @@
+<style>
+    .board-main{
+        background-color: rgba(87, 87, 87,.7)
+    }
+</style>
+<div class="relative border-2 t-20 l-20">
+    <h2 class="text-3xl text-center my-4">ようこそ！</h2>
+    <a href="" class="text-2xl text-center my-2">使い方について</a>
+    <a class="text-2xl text-center my-2">プライバシーポリシー</a>
+</div>
