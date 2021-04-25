1f93eff8380ca84939900ddb522dc33f0fbef2e3
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-04-25 09:44:29 +0900

    wip コメント機能の実装中

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Identifier;
+use App\Models\SunDock;
+use GrahamCampbell\ResultType\Result;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function post(Request $request,$uuid){
+        $this->validate($request,Comment::$rules);
+        $identifier=Identifier::where("identifier",$request->cookie("sun_dock_identifier"))->first();
+        $sunDock=SunDock::where("uuid",$uuid)->first();
+        $identifier_id=$identifier->id;
+        $sunDock_id=$sunDock->id;
+        $form=[
+            "content"=>$request->content,
+            "identifier_id"=>$identifier_id,
+            "sunDock_id"=>$sunDock_id,
+
+            'uuid'=>$uuid,
+        ];
+        Comment::create($form);
+        return redirect("/SunDock/".$uuid); 
+    }
 }
